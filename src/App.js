fe526bb5714aa0b1262507ba655f9c3153e95199
lomaster064 <64ilya64@gmail.com>
2023-11-22 19:59:11 +0400

    Navbart
    Сделали верхнее меню с лого и иконками пользователя и корзины

--- src/App.js
+++ src/App.js
@@ -1,3 +1,39 @@
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <header>
+        <div className="header_left">
+          <img src="./images/logo.svg" className="img_logo" alt="logo shop" />
+          <div className="header_info">
+            <h3> SarSeller </h3>
+            <p> Лучший магазин </p>
+          </div>
+        </div>
+
+        <div className="header_right">
+          <ul className="menu_right">
+            <li>
+              <img
+                src="./images/shop_basket.png"
+                className="img_logo"
+                alt="logo shop"
+              />
+              <span> 1000 Rub </span>
+            </li>
+
+            <li>
+              <img
+                src="./images/user_logo.png"
+                className="img_logo"
+                alt="logo shop"
+              />
+              <span> lomaster </span>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <div className="content"></div>
+    </div>
+  );
 }
